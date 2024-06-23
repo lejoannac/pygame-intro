40978d2436650aab4f5c78b6f0937a4df96b67b0
lejoannac <jxcai534@gmail.com>
2024-06-23 00:48:08 -0700

    new

--- code.py
+++ code.py
@@ -8,3 +8,13 @@ GRAFFITI_BG = 'images/bp-miller-Xeif8SdEgRU-unsplash.jpg'
 DATA_BG = 'images/markus-spiske-iar-afB0QQw-unsplash.jpg'
 
 screen = pygame.display.set_mode((500, 500))
+screen.fill((175, 217, 70))
+clock = pygame.time.Clock()
+
+# load & scale the image
+background = pygame.image.load(GRAFFITI_BG)
+background = pygame.transform.scale(background, (1500, 1500))
+
+# getting a system font
+font = pygame.font.SysFont('comicsans', 45)
+result_font = pygame.font.SysFont('comicsans', 18)
