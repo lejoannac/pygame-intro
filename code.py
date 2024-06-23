c0ff20ab254c932fa06b1a8e8659e56e78668acb
lejoannac <jxcai534@gmail.com>
2024-06-22 23:07:18 -0700

    change files

---
+++ code.py
@@ -0,0 +1,10 @@
+import pygame   # type: ignore
+import sys
+import csvReader 
+
+pygame.init()
+
+GRAFFITI_BG = 'images/bp-miller-Xeif8SdEgRU-unsplash.jpg'
+DATA_BG = 'images/markus-spiske-iar-afB0QQw-unsplash.jpg'
+
+screen = pygame.display.set_mode((500, 500))
